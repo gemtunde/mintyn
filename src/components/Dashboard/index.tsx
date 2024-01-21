7b78d60b2db1dcefcecc54f50d85e67f3ae95661
gemtunde <gemtunde@gmail.com>
2024-01-21 08:03:08 +0100

    charts

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
 import DailyTransactions from "../DailyTransactions";
+import Charts from "../Charts";
 
 type Props = {};
 
@@ -7,6 +10,10 @@ const Dashboard = (props: Props) => {
   return (
     <main className="flex min-h-screen bg-gray-100 flex-col items-center justify-between p-4">
       <DailyTransactions />
+      <div className="flex gap-2 mt-[-130px]">
+        <Charts />
+        <div className="flex flex-col gap-1"></div>
+      </div>
     </main>
   );
 };
